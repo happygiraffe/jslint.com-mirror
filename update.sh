b582470a89b2532e4877a75b7fa384eb3573aefe
Dominic Mitchell <dom@happygiraffe.net>
2009-07-24 14:26:53 +0100

    Be quiet.
    This should be run out of cron, so I don't want endless emails.

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 # Update the mirror of jslint.com.
 #
 
-wget --mirror http://www.jslint.com/
+wget --quiet --mirror http://www.jslint.com/
 git add www.jslint.com
 git commit -m 'Import latest www.jslint.com.' www.jslint.com
