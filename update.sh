1c50b5cf29cbcb8e4795f7c82265f8a38d255d8a
Dominic Mitchell <dom@happygiraffe.net>
2009-07-24 14:38:06 +0100

    Do nothing if there is nothing to commit.
    In particular, be silent for cron.

--- update.sh
+++ update.sh
@@ -3,6 +3,15 @@
 # Update the mirror of jslint.com.
 #
 
+me=`basename $0`
+tmpf=`mktemp -t $me`
+trap "rm -f $tmpf" EXIT HUP INT QUIT TERM
+
 wget --quiet --mirror http://www.jslint.com/
-git add www.jslint.com
-git commit -m 'Import latest www.jslint.com.' www.jslint.com
+
+cd www.jslint.com
+git ls-files --others --modified >$tmpf
+if [ -s $tmpf ] ; then
+    xargs git add <$tmpf
+    xargs git commit -m 'Import latest www.jslint.com.' <$tmpf
+fi
